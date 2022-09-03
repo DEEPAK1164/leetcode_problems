ca61301e3c7a541aeb61ac9f80ab09345a1cb241
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2022-09-03 19:03:04 +0530

    Time: 218 ms (86.77%), Space: 10.2 MB (70.37%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>v(n+1,true);
+        v[0]=false;
+        v[1]=false;
+        int ct=0;
+        for(int i=2;i<n;i++)
+        {
+            if(v[i])
+            {
+                ct++;
+                for(int j=2*i;j<n;j=j+i)
+                {
+                    v[j]=false;
+                }
+            }
+        }
+        return ct;
+    }
+};
