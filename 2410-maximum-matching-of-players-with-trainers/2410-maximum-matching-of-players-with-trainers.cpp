c6e9587b2c5c90fa1c8e35b7e878a6c72f980532
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2022-09-17 23:00:16 +0530

    Time: 389 ms (14.29%), Space: 76.6 MB (42.86%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-        priority_queue<int>st1;
-        priority_queue<int>st2;
-        sort(players.begin(),players.end(),greater<int>());
-        for(int i=0;i<players.size();i++)
-        {
-            st1.push(players[i]);
-        }
-           for(int i=0;i<trainers.size();i++)
-        {
-            st2.push(trainers[i]);
-        }
-        
-        int ans=0;
-        while(1)
-        {
-        
-            if(st2.empty()||st1.empty())
-            {
-            break;
-            }
-           if(st1.top()>st2.top())
-           {
-               st1.pop();
-           }
-            else if(st1.top()<=st2.top())
-            {
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        int ans = 0;
+        int i = 0, j = 0;
+        while (i < players.size() && j < trainers.size()) {
+            if (players[i] <= trainers[ j]) {
                 ans++;
-                st1.pop();
-                st2.pop();
+                i++; j++;
+            } else {
+                j++;
             }
-        }    
-        return ans;
+        }
         
+        return ans;
     }
 };
