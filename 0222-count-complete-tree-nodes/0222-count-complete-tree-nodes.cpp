fe9e2a8a61642f33f1e5e9db7fc3a1b5f1ef9846
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2022-10-15 17:20:18 +0530

    Time: 43 ms (80.04%), Space: 30.8 MB (62.88%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,10 +11,36 @@
  */
 class Solution {
 public:
+    int lftht(TreeNode *node)
+    {
+        int h=0;
+        while(node)
+        {
+            h++;
+            node=node->left;
+        }
+        return h;
+        
+    }
+    int rhtht(TreeNode *node)
+    {
+        int h=0;
+        while(node)
+        {
+            h++;
+            node=node->right;
+        }
+        return h;
+        
+    }
     int countNodes(TreeNode* root) {
            if(root==NULL)
             return 0;
-
+        int lh=lftht(root);
+        int rh=rhtht(root);
+        if(lh==rh)
+            return (1<<lh)-1;
+        
         //recursive call to left child and right child and
         // add the result of these with 1 ( 1 for counting the root)
         return 1 + countNodes(root->left) + countNodes(root->right);
