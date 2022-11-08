c4cbe33df9202f285112e7592252c21e35133570
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2022-11-08 12:09:21 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,49 +5,31 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  bool check(int src,vector<int>adj[],int vis[])
-  {
-      vis[src]=1;
-      queue<pair<int,int>>q;
-      q.push({src,-1});
-      while(!q.empty())
-      {
-          int node=q.front().first;
-          int parent=q.front().second;
-          q.pop();
-          for(auto adjacentnode:adj[node])
-          {
-              if(!vis[adjacentnode])
-              {
-              vis[adjacentnode]=1;
-              q.push({adjacentnode,node});
-              }
-                 else 
-                 {
-                     if(parent!=adjacentnode)
-                  return true;
-                 }
-        
-          }
-      }
-      
-      return false;
-      
-      
-  }
+  bool dfs(int node, int parent, int vis[], vector<int> adj[]) {
+        vis[node] = 1; 
+        // visit adjacent nodes
+        for(auto adjacentNode: adj[node]) {
+            // unvisited adjacent node
+            if(!vis[adjacentNode]) {
+                if(dfs(adjacentNode, node, vis, adj) == true) 
+                    return true; 
+            }
+            // visited node but not a parent node
+            else if(adjacentNode != parent) return true; 
+        }
+        return false; 
+    }
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
-        int vis[V]={0};
-         for(int i=0;i<V;i++)
-        {
-            if(!vis[i])
-            {
-                if(check(i,adj,vis))
-                return true;
-            }
-        }
-        return false;
+        int vis[V] = {0}; 
+       // for graph with connected components 
+       for(int i = 0;i<V;i++) {
+           if(!vis[i]) {
+               if(dfs(i, -1, vis, adj) == true) return true; 
+           }
+       }
+       return false; 
     }
 };
 
