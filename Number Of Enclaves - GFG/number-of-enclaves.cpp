33d30c4126c706a6d156f19c7dde9fefdded889f
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2022-12-09 14:49:42 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -9,61 +9,65 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    private:
-    void dfs(int r,int c,vector<vector<int>>&vis,vector<vector<int>>&mat,int dr[],int dc[])
-    {
-        int n=mat.size();
-        int m=mat[0].size();
-        vis[r][c]=1;
-        //check for top right left bottom
-        for(int i=0;i<4;i++)
-        {
-            int nbr=r+dr[i];
-            int nbc=c+dc[i];
-            if(nbr>=0&&nbr<n&&nbc>=0&&nbc<m&&!vis[nbr][nbc]&&mat[nbr][nbc]==1)
-            {
-                dfs(nbr,nbc,vis,mat,dr,dc);
-            }
-        }
-    }
   public:
     int numberOfEnclaves(vector<vector<int>> &grid) {
-        // Code here
-        int ct=0;
+        queue<pair<int,int>>q;
         int n=grid.size();
         int m=grid[0].size();
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
-        //create visited matrix
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int j=0;j<m;j++)
+         vector<vector<int>>vis(n,vector<int>(m,0));
+        // Code here
+         for(int j=0;j<m;j++)
         {
             //first row
-            if(!vis[0][j]&&grid[0][j]==1)
+            if(grid[0][j]==1)
             {
-                dfs(0,j,vis,grid,dr,dc);
+              q.push(make_pair(0,j));
+              vis[0][j]=1;
             }
             //last row
-            if(!vis[n-1][j]&&grid[n-1][j]==1)
+            if(grid[n-1][j]==1)
             {
-                dfs(n-1,j,vis,grid,dr,dc);
+                 q.push(make_pair(n-1,j));
+                     vis[n-1][j]=1;
             }
         }
         //traverse 1st and last column
         for(int i=0;i<n;i++)
         {
             //1st column
-            if(!vis[i][0]&&grid[i][0]==1)
+            if(grid[i][0]==1)
             {
-                dfs(i,0,vis,grid,dr,dc);
+                q.push(make_pair(i,0));
+                    vis[i][0]=1;
             }
             //last column
-            if(!vis[i][m-1]&&grid[i][m-1]==1)
+            if(grid[i][m-1]==1)
             {
-                dfs(i,m-1,vis,grid,dr,dc);
+                  q.push(make_pair(i,m-1));
+                  vis[i][m-1]=1;
             }
         }
-        for(int i=0;i<n;i++)
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
+       while(!q.empty())
+       {
+           int r=q.front().first;
+           int c=q.front().second;
+           q.pop();
+           //traverse all 4directions
+           for(int i=0;i<4;i++)
+           {
+               int nbr=r+dr[i];
+               int nbc=c+dc[i];
+               if(nbr>=0&&nbr<n&&nbc>=0&&nbc<m&&!vis[nbr][nbc]&&grid[nbr][nbc]==1)
+               {
+                   q.push(make_pair(nbr,nbc));
+                   vis[nbr][nbc]=1;
+               }
+           }
+       }
+       int ct=0;
+         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
@@ -74,6 +78,10 @@ class Solution {
             }
         }
         return ct;
+        
+        
+        
+        
     }
 };
 
