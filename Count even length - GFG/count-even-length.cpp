eea36b2b66970c336a07b114adc8f23ad6a93585
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-01-03 01:52:41 +0530

    Added solution - LeetHub

--- Count even length - GFG/count-even-length.cpp
+++ Count even length - GFG/count-even-length.cpp
@@ -4,43 +4,62 @@ using namespace std;
 
 // } Driver Code Ends
 
+#define mod 1000000007
 class Solution{
 	public:
-	using ll = long long int;
-	ll mod = 1e9 + 7;
-	ll modpow(ll a, ll b)
-	{
-	    ll x = 1%mod;
-	    a %= mod;
-	    while(b)
-	    {
-	        if(b&1)
-	            x = (x*a)%mod;
-	        a = (a*a)%mod;
-	        b >>= 1;
-	    }
-	    return x;
-	}
-	ll modinverse(ll a)
-	{
-	    return modpow(a,mod-2);
-	}
-	int compute_value(int n)
-	{
-		if(n==1)
-		return 2;
-		ll val=2;
-		for(ll i=1;i<n;i+=1)
-		{
-			//cout<<val<<" "<<i<<endl;
-		   val=(val%mod*((2*i)%mod+1)%mod*2)%mod;
-		   ll x=modinverse(i+1);
-		   val=(val*x)%mod;
-		}
-		return (int)val;
-	}
-}; // To store results of recursive call
+	unsigned long long power(unsigned long long x,  
 
+                                 unsigned long long  y, unsigned long long p) 
+{ 
+    unsigned long long res = 1; // Initialize result 
+    x = x % p; // Update x if it is more than or 
+    // equal to p 
+    while (y > 0)  
+    { 
+        // If y is odd, multiply x with result 
+        if (y & 1) 
+            res = (res * x) % p; 
+        // y must be even now 
+        y = y >> 1; // y = y/2 
+        x = (x * x) % p; 
+    } 
+
+    return res; 
+} 
+// Returns n^(-1) mod p 
+unsigned long long modInverse(unsigned long long n, unsigned long long p) 
+{ 
+
+    return power(n, p - 2, p); 
+} 
+// Returns nCr % p using Fermat's little 
+// theorem. 
+unsigned long long nCrModPFermat(unsigned long long n, unsigned long long r, unsigned long long p) 
+{ 
+    // If n<r, then nCr should return 0 
+    if (n < r) 
+        return 0; 
+    // Base case 
+    if (r == 0) 
+        return 1; 
+    // Fill factorial array so that we 
+    // can find all factorial of r, n 
+    // and n-r 
+    unsigned long long fac[n + 1]; 
+    fac[0] = 1; 
+    for (int i = 1; i <= n; i++) 
+        fac[i] = (fac[i - 1] * i) % p; 
+    return (fac[n] * modInverse(fac[r], p) % p 
+            * modInverse(fac[n - r], p) % p) 
+           % p; 
+} 
+	unsigned long long compute_value(unsigned long long n)
+	{
+	    // Code here
+	    unsigned long long res= nCrModPFermat(2*n,n, mod) ;
+                                 return res%mod;
+	}
+};
 
 //{ Driver Code Starts.
 int main(){
