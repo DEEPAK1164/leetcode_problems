90afa528970b2643ed417faa3378f9a863524e7f
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-01-24 03:24:09 +0530

    Time: 242 ms (42.45%), Space: 68.7 MB (48.83%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -3,11 +3,11 @@ public:
     int findJudge(int n, vector<vector<int>>& trust) {
     vector<int>count(n+1,0);
         for (auto it: trust) {
-            count[it[0]]--;
-            count[it[1]]++;
+            count[it[0]]++;
+            count[it[1]]--;
         }
         for (int i = 1; i <= n; ++i) {
-            if (count[i] == n - 1) return i;
+            if (count[i] == -1*(n - 1)) return i;
         }
         return -1;
     }
