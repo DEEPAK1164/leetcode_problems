bf65445f158a29895a55ce38aa99420927e4953d
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-03-03 11:11:35 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.cpp
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -12,16 +12,14 @@ class Solution{
     int maxSubarraySum(int arr[], int n){
         
         // Your code here
-        int maxEnding=arr[0];
-    int res=arr[0];
-    
-    for(int i=1;i<n;i++)
-    {
-        maxEnding=max(maxEnding+arr[i],arr[i]);
-        res=max(res,maxEnding);
-    }
-    
-    return res;
+        int res=arr[0];
+        int mxend=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            mxend=max(mxend+arr[i],arr[i]);
+            res=max(mxend,res);
+        }
+        return res;
         
     }
 };
