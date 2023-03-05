41e7218604d5340efae2938d5c054e63d2a0bbe1
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-03-05 10:53:13 +0530

    Time: 1995 ms (25.00%), Space: 14.2 MB (25.00%) - LeetHub

--- 2584-split-the-array-to-make-coprime-products/2584-split-the-array-to-make-coprime-products.cpp
+++ 2584-split-the-array-to-make-coprime-products/2584-split-the-array-to-make-coprime-products.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int findValidSplit(vector<int>& nums) {
-           int n=nums.size();
-         if(nums[0]==707929) return 4090;// test case failing 
-    //if(n==1)return -1;
-    int i=0,j=n-1,ans=0;
-    while(i<=ans){
-        j=n-1;
-        while(j>ans && gcd(nums[i],nums[j])==1){
-            j--;
-        }
-            i++;
-              ans=j;
-            
+    int findValidSplit(vector<int>& nums)
+    {
+        int n = nums.size();
+        if (nums[0] % nums[n - 1] == 0 ||
+            nums[n - 1] % nums[0] == 0)
+            return -1;
         
-        if(ans==n-1)return -1;
-     } 
-        return ans;
+        vector<int> far(n, -1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[j] % nums[i] == 0 ||
+                    nums[i] % nums[j] == 0) {
+                    far[i] = j;
+                }
+            }
+        }
+        
+        int farest = -1;
+        for (int i = 0; i < n - 1; ++i) {
+            farest = std::max(farest, far[i]);
+            if (farest <= i)
+                return i;
+        }
+        
+        return -1;
+        
     }
 };
