a219d15e5e5a08d93a1bd2fce05cdbb20a3a98e4
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-03-17 17:27:42 +0530

    Time: 2 ms (46.16%), Space: 6.3 MB (32.17%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n=haystack.size();
-        int m=needle.size();
-        int wsz=abs(n-m);
-
-        for(int ws=0;ws<=wsz;ws++)
-        {
-            for(int i=0;i<m;i++)
-            {
-                if(needle[i]!=haystack[ws+i])
-                {
-                    break;
-                }
-                if(i==m-1)
-                {
-                return ws;
-                }
-            }  
-        }
-  return -1;
+    #define d 256
+const int q=101;
+void RBSearch(string txt,string pat,int N, int M,vector<int>&v){
+    
+    //Compute (d^(M-1))%q
+    int h=1;
+    for(int i=1;i<=M-1;i++)
+        h=(h*d)%q;
+    
+    //Compute p and to
+    int p=0,t=0;
+    for(int i=0;i<M;i++){
+        p=(p*d+pat[i])%q;
+        t=(t*d+txt[i])%q;
+    }
+    
+    for(int i=0;i<=(N-M);i++){
+       //Check for hit
+       if(p==t){
+           bool flag=true;
+           for(int j=0;j<M;j++)
+                if(txt[i+j]!=pat[j]){flag=false;break;}
+            if(flag==true)
+                v.push_back(i);
+       }
+       //Compute ti+1 using ti
+       if(i<N-M){
+           t=((d*(t-txt[i]*h))+txt[i+M])%q;
+        if(t<0)t=t+q;
+       }
+    }
+    
+}
+    int strStr(string haystack, string needle)
+    {
+        vector<int>ans;
+        RBSearch( haystack,needle,haystack.size(), needle.size(),ans);
+        if(ans.size()==0)
+            return -1;
+        return ans[0];
+        
+        
+      
     }
 };
