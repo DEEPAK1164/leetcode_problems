6d97a3da13486313ca8cd5ba37c61b3cd24d034e
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-03-25 12:26:20 +0530

    Time: 636 ms (62.65%), Space: 197.5 MB (48.94%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
-    int dfs(int node, vector<int>adj[], vector<bool>& visit) {
-        int count = 1;
-        visit[node] = true;
-        for (int neighbor : adj[node]) {
-            if (!visit[neighbor]) {
-                count += dfs(neighbor, adj, visit);
+     int dfs(int node,vector<int>adj[],vector<bool>&vis)
+        {
+            int ct=1;
+            vis[node]=true;
+            for(auto nbr:adj[node])
+            {
+                if(!vis[nbr])
+                {
+                    ct+=dfs(nbr,adj,vis);
+                }
+            }
+         return ct;
+         
+        }
+    long long countPairs(int n, vector<vector<int>>& edges) 
+    {
+       vector<int>adj[n];
+        for(auto it:edges)
+        {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        long long szc=0;
+        long long pr=0;
+        long long nn=n;
+    vector<bool>vis(n);
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+            szc=dfs(i,adj,vis);
+            pr+=szc*(nn-szc);
             }
         }
-        return count;
-    }
-
-    long long countPairs(int n, vector<vector<int>>& edges) {
-        vector<int> adj[n];
-        for (auto edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
-        }
-
-        long long numberOfPairs = 0;
-        long long sizeOfComponent = 0;
-        long long remainingNodes = n;
-        vector<bool> visit(n);
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                sizeOfComponent = dfs(i, adj, visit);
-                numberOfPairs += sizeOfComponent * (remainingNodes - sizeOfComponent);
-            
-            }
-        }
-        return numberOfPairs/2;
+        return pr/2;
+        
+        
+        
     }
 };
