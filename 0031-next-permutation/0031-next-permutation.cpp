c1289b692e60072b96b7dd2d8947e0ebd1381654
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-04-18 15:15:01 +0530

    Time: 7 ms (39.14%), Space: 12.1 MB (76.42%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
-    void next(vector<int>& arr)
-{
-    next_permutation(arr.begin(),arr.end());
-}
- 
+    void rev(vector<int>&v,int i,int j)
+    {
+        while(i<j)
+        {
+            swap(v[i++],v[j--]);
+        }
+    }
     void nextPermutation(vector<int>& nums) {
-        next(nums);
+        int n=nums.size();
+        int ind=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                ind=i;
+                break;
+            }
+        }
+        if(ind==-1)
+        {
+            rev(nums,0,n-1);
+        }
+        else
+        {
+            for(int i=n-1;i>ind;i--)
+            {
+                if(nums[i]>nums[ind])
+                {
+                    swap(nums[i],nums[ind]);
+                    break;
+                }
+            }
+            rev(nums,ind+1,n-1);
+            
+            
+            
+        }
         
     }
 };
