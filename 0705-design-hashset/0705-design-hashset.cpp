b89913528bc1d5ceb986a01722f671fd70cf03ea
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-05-30 21:02:20 +0530

    Time: 96 ms (70.27%), Space: 49.4 MB (32.83%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,47 +1,51 @@
-class MyHashSet {
+class MyHashSet
+{
     int bucketSize;
-    vector<vector<int>>bucket;
+    vector<vector < int>> bucket;
     int hashFun(int key)
     {
-        return key%bucketSize;
+        return key % 10001;
     }
-public:  
-    
-    MyHashSet() {
-     bucketSize=10001;
-        bucket=vector<vector<int>>(bucketSize,vector<int>{});
-    }
-    
-    void add(int key) {
-        int i=hashFun(key);
-        if(find(bucket[i].begin(),bucket[i].end(),key)==bucket[i].end())
+    public:
+
+        MyHashSet()
+        {
+            bucketSize = 10001;
+            bucket = vector<vector < int>> (bucketSize, vector < int> {});
+        }
+
+    void add(int key)
+    {
+        int i = hashFun(key);
+        if (find(bucket[i].begin(), bucket[i].end(), key) == bucket[i].end())
         {
             bucket[i].push_back(key);
         }
-        
     }
-    
-    void remove(int key) {
-       int i=hashFun(key); 
-        auto it=find(bucket[i].begin(),bucket[i].end(),key);
-        if(it!=bucket[i].end()) 
+
+    void remove(int key)
+    {
+        int i = hashFun(key);
+        auto it = find(bucket[i].begin(), bucket[i].end(), key);
+        if (it != bucket[i].end())
         {
-            bucket[i].erase(it); 
+            bucket[i].erase(it);
         }
     }
-    
-    bool contains(int key) {
-          int i=hashFun(key); 
-         if(find(bucket[i].begin(),bucket[i].end(),key)==bucket[i].end())
-             return false;
+
+    bool contains(int key)
+    {
+        int i = hashFun(key);
+        if (find(bucket[i].begin(), bucket[i].end(), key) == bucket[i].end())
+            return false;
         return true;
     }
 };
 
 /**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
+ *Your MyHashSet object will be instantiated and called as such:
+ *MyHashSet* obj = new MyHashSet();
+ *obj->add(key);
+ *obj->remove(key);
+ *bool param_3 = obj->contains(key);
  */
