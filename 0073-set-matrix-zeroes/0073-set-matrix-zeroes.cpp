5ad5ee5921d3e927143b552384029ccdf5ed0bc2
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-06-12 02:17:21 +0530

    Time: 22 ms (14.81%), Space: 13.5 MB (15.07%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-       vector<int>matrow(n,0);
-        vector<int>matcol(m,0);
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    matrow[i]=1;
-                    matcol[j]=1;
-                }
-            }
+         int R = matrix.size();
+    int C = matrix[0].size();
+    set<int>rows;
+    set<int>cols;
+
+    // Essentially, we mark the rows and columns that are to be made zero
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (matrix[i][j] == 0) {
+          rows.insert(i);
+          cols.insert(j);
         }
-        for(int i=0;i<n;i++)
-        {
-        for(int j=0;j<m;j++)
-        {
-            
-                if(matrow[i]||matcol[j])
-                {
-                    matrix[i][j]=0;
-                }
-                
-           
+      }
+    }
+
+    // Iterate over the array once again and using the rows and cols sets, update the elements.
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        if (rows.count(i)==1 || cols.count(j)==1) {
+          matrix[i][j] = 0;
         }
-        }
+      }
+    }
     }
 };
