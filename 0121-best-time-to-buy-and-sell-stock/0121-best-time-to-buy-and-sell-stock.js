72ca6632c50c51825bdb96a4e4e6d0e53d54077b
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-06-18 17:42:41 +0530

    Time: 73 ms (93.45%), Space: 51.9 MB (19.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,15 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-     var mn=10001;
-        var n=prices.length;
-        var p=0;
-        for(let i=0;i<n;i++)
+  let n=prices.length;;
+    let mx=0;
+    let mn=prices[0];
+    for(let i=1;i<n;i++)
         {
-        
-            mn=(mn<=prices[i])?mn:prices[i];
-            p=(p>=prices[i]-mn ?p:prices[i]-mn);
-        
+            mx=Math.max(mx,prices[i]-mn);
+            mn=Math.min(mn,prices[i]);
         }
-        return p;
+    return mx;
 };
