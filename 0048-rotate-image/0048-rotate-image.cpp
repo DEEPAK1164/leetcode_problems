8b48573ca4b3ce42d0d0ce4051849672e1f92ef8
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-06-18 23:36:31 +0530

    Time: 0 ms (100.00%), Space: 7 MB (54.28%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        int r=matrix.size();
+        int c=matrix[0].size();
         int n=matrix.size();
         for(int i=0;i<n;i++)
         {
@@ -9,9 +11,9 @@ public:
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0;i<n;i++)
+        for(int row=0;row<r;row++)
         {
-            reverse(matrix[i].begin(),matrix[i].end());
-}
+           reverse(matrix[row].begin(),matrix[row].end());
+        }
     }
 };
