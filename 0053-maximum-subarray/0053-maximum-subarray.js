a19a9fbdb9270ceeeaac7312ed834e599bcf38fa
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-06-18 10:10:49 +0530

    Time: 79 ms (70.85%), Space: 50.3 MB (57.23%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let mx = -1e4-1;
+     let sm = 0;
+    let n=nums.length;
+    for(let i=0;i<n;i++)
+        {
+            sm+=nums[i];
+            mx=Math.max(sm,mx);
+                if(sm<0)
+                {
+                    sm=0;
+                }
+        }
+    return mx;
+};
