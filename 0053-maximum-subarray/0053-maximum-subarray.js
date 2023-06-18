7a6901bf6f935c1060263dd37de6c392476102b6
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-06-18 10:11:36 +0530

    Time: 80 ms (66.92%), Space: 50 MB (86.19%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let mx = -1e4-1;
+    let mx = Number.MIN_SAFE_INTEGER;
      let sm = 0;
     let n=nums.length;
     for(let i=0;i<n;i++)
         {
             sm+=nums[i];
             mx=Math.max(sm,mx);
-                if(sm<0)
+            if(sm<0)
                 {
                     sm=0;
                 }
