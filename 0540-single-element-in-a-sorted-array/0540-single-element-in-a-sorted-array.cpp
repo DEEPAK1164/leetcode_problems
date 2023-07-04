b47a8138d4d9094ee00d37554e547e5aeaf97d02
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-07-04 21:23:57 +0530

    Time: 34 ms (25.98%), Space: 22.2 MB (79.54%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int l=0,h=nums.size()-2,mid;
         while(l<=h)
         {
-            mid=(l+h)>>1;
+            mid=l+(h-l)/2;
             if(mid%2==1)
             {
                 if(nums[mid-1]!=nums[mid])
