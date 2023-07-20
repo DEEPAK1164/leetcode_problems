4e1945c0abffa63e32686b4f88186b053e638cc3
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-07-21 00:32:55 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,31 +5,49 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  bool dfs(int node, int parent, int vis[], vector<int> adj[]) {
-        vis[node] = 1; 
-        // visit adjacent nodes
-        for(auto adjacentNode: adj[node]) {
-            // unvisited adjacent node
-            if(!vis[adjacentNode]) {
-                if(dfs(adjacentNode, node, vis, adj) == true) 
-                    return true; 
-            }
-            // visited node but not a parent node
-            else if(adjacentNode != parent) return true; 
-        }
-        return false; 
-    }
     // Function to detect cycle in an undirected graph.
-    bool isCycle(int V, vector<int> adj[]) {
+    bool iscycle(int src, vector<int> adj[],vector<int>&vis)
+    {
+        queue<pair<int,int>>q;
+        q.push({src,-1});
+        vis[src]=1;
+        while(!q.empty())
+        {
+            int node=q.front().first;
+            int par=q.front().second;
+            q.pop();
+            for(auto it:adj[node])
+            {
+                if(!vis[it])
+                {
+                    vis[it]=1;
+                    q.push({it,node});
+                }
+                else if(par!=it)
+                    {
+                        return true;
+                    }
+            }
+        }
+        return false;
+    }
+    bool isCycle(int V, vector<int> adj[]) 
+    {
         // Code here
-        int vis[V] = {0}; 
-       // for graph with connected components 
-       for(int i = 0;i<V;i++) {
-           if(!vis[i]) {
-               if(dfs(i, -1, vis, adj) == true) return true; 
-           }
-       }
-       return false; 
+        vector<int>vis(V,0);
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                if(iscycle(i,adj,vis))
+                {
+                return true;
+                }
+            }
+        }
+        
+        return false;
+        
     }
 };
 
