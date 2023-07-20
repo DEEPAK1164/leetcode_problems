0a8453b57e7482b881f021c0eff960ca3afe79a3
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-07-20 21:01:23 +0530

    Time: 3 ms (98.97%), Space: 13.9 MB (86.72%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    void dfs(vector<vector<int>>& image,vector<vector<int>>& ans, int sr, int sc, int color,int dr[],int dc[],int ic)
     {
         int m=image.size();
         int n=image[0].size();
-        int clr=image[sr][sc];
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        int dr[]={1,0,-1,0};
-        int dc[]={0,-1,0,1};
-     vector<vector<bool>> visited(m, vector<bool>(n, false));
-        while(!q.empty())
+       ans[sr][sc]=color;
+       
+        for(int i=0;i<4;i++)
         {
-            int r=q.front().first;
-            int c=q.front().second;
-              image[r][c]=color;
-            visited[r][c] = true;
-               q.pop();
-            for(int i=0;i<4;i++)
+            int nr=sr+dr[i];
+            int nc=sc+dc[i];
+            if(nr>=0&&nr<m&&nc>=0&&nc<n&&ans[nr][nc]!=color&&image[nr][nc]==ic)
             {
-                int nr=r+dr[i];
-                int nc=c+dc[i];
-                if(nr>=0&&nr<m&&nc>=0&&nc<n&&image[nr][nc]==clr&&!visited[nr][nc])
-                {
-                  
-                    q.push({nr,nc});
-                }
-                
-            }
-        }
-        return image;   
+                dfs(image,ans,nr,nc,color,dr,dc,ic);
+            }    
+        }  
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)
+    {
+        vector<vector<int>>ans=image;
+        int ic=image[sr][sc];
+        int dr[]={1,0,-1,0};
+        int dc[]={0,1,0,-1};
+        dfs(image,ans,sr,sc,color,dr,dc,ic); 
+        return ans;     
     }
 };
