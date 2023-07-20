1d2b52ab8bdd9bdd64191a21536247c326479b5b
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-07-20 17:36:46 +0530

    Time: 15 ms (71.71%), Space: 17.8 MB (55.66%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,41 +1,56 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
-        
-        for (int asteroid : asteroids) {
-            int flag = 1;
-            while (!st.empty() && (st.top() > 0 && asteroid < 0)) {
-                // If the top asteroid in the stack is smaller, then it will explode.
-                // Hence pop it from the stack, also continue with the next asteroid in the stack.
-                if (abs(st.top()) < abs(asteroid)) {
-                    st.pop();
-                    continue;
-                }
-                // If both asteroids are the same size, then both asteroids will explode.
-                // Pop the asteroid from the stack; also, we won't push the current asteroid to the stack.
-                else if (abs(st.top()) == abs(asteroid)) {
-                    st.pop();
-                }
-
-                // If we reach here, the current asteroid will be destroyed
-                // Hence, we should not add it to the stack
-                flag = 0;
-                break;
-            }
+    stack<int>st;
+        for(auto x:asteroids)
+        {
             
-            if (flag) {
-                st.push(asteroid);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            if(x>0)
+            {
+                st.push(x);
+            }
+            else
+            {
+                while(!st.empty()&&st.top()>0&&st.top()<-x)
+                {
+                    st.pop();
+                }
+                if(!st.empty()&&st.top()==-x)
+                {
+                    st.pop();
+                }
+                else if(!st.empty() && st.top()>-x)
+                {
+                    
+                }
+            else {
+                st.push(x);
+            }
+                
             }
         }
-        
-        // Add the asteroids from the stack to the vector in the reverse order.
-        vector<int> remainingAsteroids (st.size());
-        for (int i = remainingAsteroids.size() - 1; i >= 0; i--){
-            remainingAsteroids[i] = st.top();
+        vector<int>v;
+        while(!st.empty())
+        {
+            v.push_back(st.top());
             st.pop();
         }
-        
-        return remainingAsteroids;
+        reverse(v.begin(),v.end());
+        return v;
     }
 };
