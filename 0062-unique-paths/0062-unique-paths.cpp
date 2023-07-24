0914c4c51b0f25b64271aade8dcd8d66f99540e0
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-07-24 20:25:10 +0530

    Time: 5 ms (22.88%), Space: 6.6 MB (19.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
-    int ncr(int n,int r)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    int rec(int m,int n,vector<vector<int>>&dp)
     {
-        
-        double res=1;
-      for (int i = 1; i <= r; i++) {
-        res = res * (n-r +i);
-        res = res / (i );
-    }
-    return (int)res;
-        
-    }
-    int uniquePaths(int m, int n)
-    {
-        if(n==1||m==1)
+    
+    
+        if(m==0&&n==0)
+        {
             return 1;
-        return ncr((m+n-2),(m-1));
+        }
+        if(m<0||n<0)
+        {
+            return 0;
+        }
+       
+        if(dp[m][n]!=-1)
+        {
+            return dp[m][n];
+        }
+        
+        return dp[m][n]= rec(m-1,n,dp)+rec(m,n-1,dp);
+
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return rec(m-1,n-1,dp);
     }
 };
