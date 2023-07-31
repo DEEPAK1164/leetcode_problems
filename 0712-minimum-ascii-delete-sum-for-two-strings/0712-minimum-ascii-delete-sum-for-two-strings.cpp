09dfd500518166206411391f3ec743b96915d698
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-07-31 17:00:19 +0530

    Time: 52 ms (59.01%), Space: 15.4 MB (14.89%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,28 +1,28 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-
 class Solution {
 public:
-    int minimumDeleteSum(std::string s1, std::string s2) {
+    int minimumDeleteSum(string s1, string s2) {
         int m = s1.length();
         int n = s2.length();
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        return rec(s1, s2, m - 1, n - 1, dp);
+        return rec(s1, s2, m-1, n-1, dp);
     }
 
 private:
     int rec(string& s1, string& s2, int i, int j, vector<vector<int>>& dp) {
-       
-        if (i <0) {
+        if(i<0&&j<0)
+            return 0;
+        
+        if (i < 0) {
+            
             int c = 0;
             for (int k = 0; k <= j; k++) {
-                c += s2[k];
+            c += s2[k];
             }
             return c;
         }
 
-        if (j <0) {
+        if (j < 0) {
+        
             int c = 0;
             for (int k = 0; k <= i; k++) {
                 c += s1[k];
@@ -32,15 +32,18 @@ private:
 
         if (s1[i] == s2[j]) {
             return rec(s1, s2, i - 1, j - 1, dp);
-        }
-
-        if (dp[i][j] != -1) {
+        } 
+         if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        int take1 = s1[i] + rec(s1, s2, i - 1, j, dp);
-        int take2 = s2[j] + rec(s1, s2, i, j - 1, dp);
+              int take1 = s1[i]+rec(s1 , s2 , i-1 , j,dp);
 
-        return dp[i][j] =min(take1, take2);
+    int take2 = s2[j]+rec(s1 , s2 , i , j-1,dp);
+
+    return dp[i][j]= min(take1 , take2);
+
+      
+        return dp[i][j];
     }
 };
