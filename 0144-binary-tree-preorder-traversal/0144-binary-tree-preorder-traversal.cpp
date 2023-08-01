b780ce461c2d69a2846badbe9951edae9a331228
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-08-01 12:14:23 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (59.74%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,25 +1,40 @@
-
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void print(TreeNode* root,vector<int>&v)
-    {
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
+        st.push(root);
+        vector<int>v;
         if(root==NULL)
         {
-            return;
+            return v;
         }
-        v.push_back(root->val);
-        print(root->left,v);
-         print(root->right,v);
+        while(!st.empty())
+        {
+            root=st.top();
+             v.push_back(root->val);
+            st.pop();
+            if(root->right)
+            {
+                st.push(root->right);
+                
+            }
+             if(root->left)
+            {
+                st.push(root->left);
+            }
+        }
         
-        
-        
-        
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>v;
-     print(root,v);
         return v;
-     
     }
 };
