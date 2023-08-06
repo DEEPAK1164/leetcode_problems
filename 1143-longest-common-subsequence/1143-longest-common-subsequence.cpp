44c77c7765a763a02bb40fd7fc098466d889b5a1
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-08-07 01:59:01 +0530

    Time: 64 ms (58.50%), Space: 24.6 MB (25.81%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int rec(string &s1,int id1,string &s2, int id2,vector<vector<int>>&dp)
-    {
-        if(id1<0||id2<0)
+    int rec(const string& s1, int id1, const string& s2, int id2, vector<vector<int>>& dp) {
+        if (id1 < 0 || id2 < 0)
             return 0;
-        if(dp[id1][id2]!=-1)
+        
+        if (dp[id1][id2] != -1)
             return dp[id1][id2];
-        if(s1[id1]==s2[id2])
-            return dp[id1][id2]= 1+rec(s1,id1-1,s2,id2-1,dp);
-        return dp[id1][id2]=0+max(rec(s1,id1-1,s2,id2,dp),rec(s1,id1,s2,id2-1,dp));
-    
+        
+        if (s1[id1] == s2[id2])
+            return dp[id1][id2] = 1 + rec(s1, id1 - 1, s2, id2 - 1, dp);
+        
+        return dp[id1][id2] = max(rec(s1, id1 - 1, s2, id2, dp), rec(s1, id1, s2, id2 - 1, dp));
     }
+    
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        int id1=text1.size()-1;
-        int id2=text2.size()-1;
-        int ans=rec(text1,id1,text2,id2,dp);
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int ans = rec(text1, n - 1, text2, m - 1, dp);
         return ans;
     }
 };
