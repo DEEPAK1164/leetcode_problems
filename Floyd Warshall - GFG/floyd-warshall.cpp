7a9c3d24d2cc311e2767bc3260218228a1c1fab2
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-08-12 16:38:55 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -5,44 +5,38 @@
 using namespace std;
 
 // } Driver Code Ends
-//User function template for C++
+//Back-end complete function template for C++
 
 class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&matrix){
-	    // Code here
-	    int n = matrix.size();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == -1) {
-					matrix[i][j] = 1e9;
-				}
-				if (i == j) matrix[i][j] = 0;
+		int INF = INT_MAX;
+		int n = matrix.size();
+		
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(matrix[i][j] == -1)
+					matrix[i][j] = INF;
 			}
 		}
-
-		for (int k = 0; k < n; k++) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					matrix[i][j] = min(matrix[i][j],
-					                   matrix[i][k] + matrix[k][j]);
-				}
-			}
+		
+		for (int k = 0; k < n; ++k) {
+		    for (int i = 0; i < n; ++i) {
+		        for (int j = 0; j < n; ++j) {
+		            if (matrix[i][k] < INF && matrix[k][j] < INF)
+		                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]); 
+		        }
+		    }
 		}
-
-
-
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == 1e9) {
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(matrix[i][j] >= INF)
 					matrix[i][j] = -1;
-				}
 			}
 		}
-	
 	}
 };
+
 
 //{ Driver Code Starts.
 int main(){
