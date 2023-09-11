e27ee9818d99c2d7b55f6d40c0037244b9eaa8f3
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-09-11 19:14:01 +0530

    Time: 0 ms (100.00%), Space: 13.8 MB (5.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,22 @@
 class Solution {
 public:
-    int left(vector<int>&v,int l,int r,int x)
-    {
-        int mid=l+(r-l)/2;
-        if(l>r)
-            return -1;
-        if(v[mid]>x)
-            return left(v,l,mid-1,x);
-        else if(v[mid]<x)
-            return left(v,mid+1,r,x);
-        else
-        {
-            if(mid==0||v[mid]!=v[mid-1])
-            return mid;
-            else
-                return left(v,l,mid-1,x);
-        } 
-    }
-     int right(vector<int>&v,int l,int r,int x)
-            {
-                
-        int mid=l+(r-l)/2;
-        if(l>r)
-            return -1;
-        if(v[mid]>x)
-            return right(v,l,mid-1,x);
-        else if(v[mid]<x)
-            return right(v,mid+1,r,x);
-        else
-        {
-            if(mid==v.size()-1||v[mid]!=v[mid+1])
-            return mid;
-            else
-                return right(v,mid+1,r,x);
-        } 
-            }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int>v;
+        int lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int ub=upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1;
         
-    vector<int> searchRange(vector<int>& nums, int target) 
-    {
-        vector<int>ans;
-        int xx=left(nums,0,nums.size()-1,target);
-        int yy=right(nums,0,nums.size()-1,target);
-        ans.push_back(xx);
-        ans.push_back(yy);
-        return ans;
+        if(lb==n || nums[lb]!=target)
+        {
+            v.push_back(-1);
+             v.push_back(-1);
+        }
+        else
+        {
+            v.push_back(lb);
+             v.push_back(ub);
+        }
+        
+        return v;
     }
 };
