f09a41236a036d0626adc809684ec6ff092c9b0d
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-10-21 12:27:49 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,48 +7,37 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	
-	
-	
-	
-	
-	void dfs(int node, vector<int> adj[],vector<int>&vis,stack<int>&st)
+	void dfs(int node, vector<int> adj[],stack<int>&st,vector<bool>&vis)
 	{
+	    
 	    vis[node]=1;
-	    for (auto it:adj[node])
-      {
-    if(!vis[it])
-    {
-        dfs(it,adj,vis,st);
-    }
-       }	    
+	    for(auto it:adj[node])
+	    {
+	        if(!vis[it])
+	        {
+	            dfs(it,adj,st,vis);
+	        }
+	    }
 	    st.push(node);
 	    
-	    
 	}
-	
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
+	    vector<int>v;
 	    stack<int>st;
-	    vector<int>vis(V,0);
+	    vector<bool>vis(V,0);
 	    for(int i=0;i<V;i++)
 	    {
 	        if(!vis[i])
-	        {
-	            dfs(i,adj,vis,st);
-	        }
+	        dfs(i,adj,st,vis);
 	    }
-	    vector<int>v;
 	    while(!st.empty())
 	    {
 	        v.push_back(st.top());
 	        st.pop();
 	    }
 	    return v;
-	    
-	    
-	    
 	}
 };
 
