984be33f6e8b48e44e1e4049db40c6de99b0b3f5
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-10-26 23:19:17 +0530

    Added solution - LeetHub

---
+++ Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+//Function to find the maximum possible amount of money we can win.
+
+class Solution {
+public:
+    long long f(int arr[], int i, int j, int sm, vector<vector<int>>& dp) {
+        if (j == i + 1)
+            return max(arr[i], arr[j]);
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        long long option1 = sm - f(arr, i + 1, j, sm - arr[i],dp);
+        long long option2 = sm - f(arr, i, j - 1, sm - arr[j],dp);
+
+        dp[i][j] = max(option1, option2);
+        return dp[i][j];
+    }
+
+    long long maximumAmount(int arr[], int n) {
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        int i = 0, j = n - 1;
+        int sm = 0;
+        for (int i = 0; i < n; i++) {
+            sm += arr[i];
+        }
+
+        return f(arr, 0, n - 1, sm, dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		cout<< ob.maximumAmount(a,n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
