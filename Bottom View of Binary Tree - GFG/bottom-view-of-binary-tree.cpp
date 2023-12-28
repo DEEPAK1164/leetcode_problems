0e299b1fe9ece510e64da0686ea59590d446d739
DEEPAK_MAURYA <85310059+DEEPAK1164@users.noreply.github.com>
2023-12-28 10:39:56 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
@@ -95,34 +95,36 @@ Node* buildTree(string str)
 
 class Solution {
   public:
+  
     vector <int> bottomView(Node *root) {
         // Your Code Here
-          vector<int>ans;
-        if(root==NULL)
-        return ans;
-        queue<pair<Node*,int>>q;
-        q.push({root,0}); //node,line
-        map<int,int>mp;//line,node
+        queue<pair<Node*, int>>q;
+        map<int, Node*>mp;
+        q.push({root,0});
         while(!q.empty())
         {
             auto it=q.front();
-               q.pop();
-            int line=it.second;
+            q.pop();
             Node* node=it.first;
-                mp[line]=node->data;
-    
-            if(node->left)
-            q.push({node->left,line-1});
-            if(node->right)
-            q.push({node->right,line+1});            
+            int line=it.second;
+            mp[line]=node;
+            if(node->left!=NULL)
+            {
+                q.push({node->left,line-1});
+            }
+            
+            if(node->right!=NULL){
+                 q.push({node->right,line+1});
+            }
         }
-    
-        for(auto it:mp)
-        {
-            ans.push_back(it.second);
-        }
+                vector<int>v;
+               for(auto it:mp)
+              {
+                v.push_back(it.second->data);
+              }
+                      return v;
         
-        return ans;
+        
     }
 };
 
